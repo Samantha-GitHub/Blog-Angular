4a364bee28b22d23b94a4ff319f64e64efc24324
Samantha-Bitbucket <ottavianisamantha@gmail.com>
2021-02-21 16:13:04 +0100

    pipes fecha anadido + borrado del validator fecha

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -48,7 +48,6 @@ export class FormularioComponent implements OnInit {
       fecha: new FormControl('', [
 
         Validators.required,
-        Validators.pattern(/^(?=\d)(?:(?:31(?!.(?:0?[2469]|11))|(?:30|29)(?!.0?2)|29(?=.0?2.(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00)))(?:\x20|$))|(?:2[0-8]|1\d|0?[1-9]))([-./])(?:1[012]|0?[1-9])\1(?:1[6-9]|[2-9]\d)?\d\d(?:(?=\x20\d)\x20|$))?(((0?[1-9]|1[012])(:[0-5]\d){0,2}(\x20[AP]M))|([01]\d|2[0-3])(:[0-5]\d){1,2})?$/),
 
       ]),
       categoria: new FormControl('', [
@@ -68,6 +67,12 @@ export class FormularioComponent implements OnInit {
   }
 
   onSubmit() {
+
+
+
+    /* Descomentar para siempre poner la fecha al dia de publicacion del Post
+    
+    this.formulario.value.fecha = new Date(); */
 
     this.postService.agregarPost(this.formulario.value);
     console.log(this.formulario.value);
